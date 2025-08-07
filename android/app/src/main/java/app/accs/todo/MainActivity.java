701d9a755dad46372fdbd52f6d9718dc065e5155
axhilxif <cloudfireinternational@gmail.com>
2025-08-07 18:32:34 +0530

    Re-added android and dist folders with config

---
+++ android/app/src/main/java/app/accs/todo/MainActivity.java
@@ -0,0 +1,5 @@
+package app.accs.todo;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,10 +4,7 @@ const config: CapacitorConfig = {
   appId: 'app.accs.todo',
   appName: 'MyFocus',
   webDir: 'dist',
-  server: {
-    url: 'https://6f845542-99c3-464b-8623-82e273d23ca7.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  },
+  bundledWebRuntime: false,
   plugins: {
     LocalNotifications: {
       smallIcon: "ic_stat_icon_config_sample",
